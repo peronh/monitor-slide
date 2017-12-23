01640c2f4ff404b724459ed764649ed8f7470473
pronyecz <pronyecz@gmail.com>
2017-12-23 20:06:41 +0100

    add monitor-slide html,css,jquery files

---
+++ slide.js
@@ -0,0 +1,18 @@
+$(function(){
+	
+	var currentSlide = 1;
+
+	setInterval(function(){
+
+    $('#slider #slides').animate({
+          'margin-left':'-=764px'
+    },1500,function(){
+
+    	currentSlide++;
+    	if (currentSlide === $('.slide').length ){
+    		currentSlide = 1;
+    		$('#slides').css({'margin-left':0});
+    	}
+    });   
+  },4000);
+}); 
